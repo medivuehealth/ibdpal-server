cdbefb6c3fb8ef077673f8715b39f7a2e8032f8b
SaKumar <sakumar@contractor.epri.com>
2025-07-20 15:46:03 -0400

    Simplify INSERT statement to match parameter count exactly

--- routes/journal.js
+++ routes/journal.js
@@ -57,11 +57,9 @@ async function createJournalEntry(journalData, res) {
             dinner_calories, dinner_protein, dinner_carbs, dinner_fiber, dinner_fat,
             snack_calories, snack_protein, snack_carbs, snack_fiber, snack_fat
         )
-        VALUES (
-            $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14,
-            $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29,
-            $30, $31, $32, $33, $34, $35, $36, $37, $38, $39, $40, $41, $42, $43, $44, $45, $46, $47, $48, $49
-        )
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14,
+                $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29,
+                $30, $31, $32, $33, $34, $35, $36, $37, $38, $39, $40, $41, $42, $43, $44, $45, $46, $47, $48, $49)
         RETURNING entry_id;
     `;
 
